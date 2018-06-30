0f6a17d274f2de2821bc6c236dd02be625ed1bf8
Greg <kulinski.grzegorz90@gmail.com>
2018-06-30 13:41:13 +0200

    połączenie z Backendem

---
+++ src/app/_services/authentication.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string) {
+    const header = new HttpHeaders().set('Authorization', 'Bearer').set( 'Content-Type', 'application/json');
+    return this.http.post<any>('http://localhost:8080/token/generate-token', { username: username, password: password }, {headers: header})
+      .pipe(map((res: any) => {
+        console.log('dupcia');
+        // login successful if there's a jwt token in the response
+        if (res && res.token) {
+          // store username and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem('currentUser', JSON.stringify({ username, token: res.token }));
+        }
+      }));
+  }
+
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
+  }
+}
+
+
